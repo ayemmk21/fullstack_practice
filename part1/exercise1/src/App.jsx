f61f2fa97b08122bdbc4ddbced96056aeea70d55
ammk21 <ayemohmohkhin001@gmail.com>
2025-04-07 18:13:33 +0700

    uploaded 1.3

--- part1/exercise1/src/App.jsx
+++ part1/exercise1/src/App.jsx
@@ -1,4 +1,7 @@
+/*
+
 const Header = (props) => {
+  console.log(props)
   return(
     <h1>{props.course}</h1>
   )
@@ -44,3 +47,32 @@ const App = () => {
 }
 
 export default App
+*/
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>{part1.name} exercises: {part1.exercises}</p>
+      <p>{part2.name} exercises: {part2.exercises}</p>
+      <p>{part3.name} exercises: {part3.exercises}</p>
+    </div>
+  )
+}
+
+export default App
+
