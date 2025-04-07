613029c3d22bbc03a825471249490b1225560d61
ammk21 <ayemohmohkhin001@gmail.com>
2025-04-07 18:20:40 +0700

    uploaded 1.4

--- part1/exercise1/src/App.jsx
+++ part1/exercise1/src/App.jsx
@@ -1,5 +1,5 @@
+//1.1 + 1.2
 /*
-
 const Header = (props) => {
   console.log(props)
   return(
@@ -49,6 +49,8 @@ const App = () => {
 export default App
 */
 
+//1.3
+/*
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = {
@@ -67,12 +69,43 @@ const App = () => {
   return (
     <div>
       <h1>{course}</h1>
-      <p>{part1.name} exercises: {part1.exercises}</p>
-      <p>{part2.name} exercises: {part2.exercises}</p>
-      <p>{part3.name} exercises: {part3.exercises}</p>
+      <p>{part1.name}: {part1.exercises}</p>
+      <p>{part2.name}: {part2.exercises}</p>
+      <p>{part3.name}: {part3.exercises}</p>
+      <p>Number of exercises: {part1.exercises + part2.exercises + part3.exercises}</p>
     </div>
   )
 }
 
 export default App
+*/
 
+//1.4
+const App = () => {
+  const course = 'Half Stack application development'
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>{parts[0].name}: {parts[0].exercises}</p>
+      <p>{parts[1].name}: {parts[1].exercises}</p>
+      <p>{parts[2].name}: {parts[2].exercises}</p>
+      <p>Number of exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </div>
+  )
+}
+export default App
