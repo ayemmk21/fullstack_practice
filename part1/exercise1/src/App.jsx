a84d239692ab6bc727edddd7524dabf8c127cbc1
ammk21 <ayemohmohkhin001@gmail.com>
2025-04-07 18:26:06 +0700

    uploaded 1.5

--- part1/exercise1/src/App.jsx
+++ part1/exercise1/src/App.jsx
@@ -81,6 +81,7 @@ export default App
 */
 
 //1.4
+/*
 const App = () => {
   const course = 'Half Stack application development'
   const parts = [
@@ -105,6 +106,39 @@ const App = () => {
       <p>{parts[1].name}: {parts[1].exercises}</p>
       <p>{parts[2].name}: {parts[2].exercises}</p>
       <p>Number of exercises: {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p>
+    </div>
+  )
+}
+export default App
+*/
+
+//1.5
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <h1>{course.name}</h1>
+      <p>{course.parts[0].name}: {course.parts[0].exercises}</p>
+      <p>{course.parts[1].name}: {course.parts[1].exercises}</p>
+      <p>{course.parts[2].name}: {course.parts[2].exercises}</p>
+      <p>Number of exercises: {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
     </div>
   )
 }
